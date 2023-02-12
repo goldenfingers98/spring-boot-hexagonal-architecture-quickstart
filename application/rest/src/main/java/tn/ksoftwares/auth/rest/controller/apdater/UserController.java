0bff3fbb863af8a4ba6cc4e3f15d6596bffb012a
goldenfingers98 <tchtourou21@gmail.com>
2023-02-12 18:44:49 +0100

    HEXAG: Add healthcheck.

--- application/rest/src/main/java/tn/ksoftwares/auth/rest/controller/apdater/UserController.java
+++ application/rest/src/main/java/tn/ksoftwares/auth/rest/controller/apdater/UserController.java
@@ -2,12 +2,16 @@ package tn.ksoftwares.auth.rest.controller.apdater;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import tn.ksoftwares.auth.domain.model.pojo.User;
 
 public interface UserController {
+
+    @GetMapping("/")
+    ResponseEntity<Void> healthCheck();
 
     @PostMapping("/add")
     ResponseEntity<Void> addUser(@RequestBody User user);
--- application/rest/src/main/java/tn/ksoftwares/auth/rest/controller/implementation/UserControllerImpl.java
+++ application/rest/src/main/java/tn/ksoftwares/auth/rest/controller/implementation/UserControllerImpl.java
@@ -1,5 +1,6 @@
 package tn.ksoftwares.auth.rest.controller.implementation;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ import tn.ksoftwares.auth.domain.logic.ports.api.UserService;
 public class UserControllerImpl implements UserController {
 
     @Autowired
+    private Logger logger;
+
+    @Autowired
     private UserService userService;
 
     @Override
@@ -33,5 +37,11 @@ public class UserControllerImpl implements UserController {
             throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
         }
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    public ResponseEntity<Void> healthCheck() {
+        logger.debug("health check.");
+        return null;
     }
 }
